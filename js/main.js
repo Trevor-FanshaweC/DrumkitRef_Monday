96571fb5391d91dfd8227c5fcb506a6e46e8a1cf
Trevor - Fanshawe <tvanrys@fanshaweonline.ca>
2023-03-06 15:43:16 -0500

    starting point

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// drum kit: press a key to play the sound. use JavaScript to retrieve the data-key attribute and then use that as a selector to find the matching audio file and play it!
+
+(() => {
+    console.log('music player script file');
+    
+})();
